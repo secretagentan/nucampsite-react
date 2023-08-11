f357999db070e66e39c87dc5fe452213dc44f3bd
an <anthedev@gmail.com>
2023-08-11 11:08:20 -0700

    CampsitesDirectoryPage and CampsiteDetail

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import './App.css';
 import Header from './components/Header';
-import CampsitesList from './features/campsites/CampsitesList';
 import Footer from './components/Footer';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <CampsitesList />
-      <Footer />
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Header />
+            <CampsitesDirectoryPage />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
