277263ca9a38e77ac2438971feb27613c986feaa
an <anthedev@gmail.com>
2023-08-12 07:02:43 -0700

    CampsiteDetailPage with useParams

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
+
 
 function App() {
     return (
@@ -14,6 +16,10 @@ function App() {
           <Route path='/' element={<HomePage />} />
           <Route path='contact' element={<ContactPage />} />
           <Route path='directory' element={<CampsitesDirectoryPage />} />
+          <Route
+            path='directory/:campsiteId'
+            element={<CampsiteDetailPage />}
+          />
         </Routes>
         <Footer />
       </div>
--- src/features/campsites/CampsitesList.js
+++ src/features/campsites/CampsitesList.js
@@ -2,25 +2,19 @@ import CampsiteCard from './CampsiteCard';
 import {Col, Row} from 'reactstrap';
 import { selectAllCampsites } from './campsitesSlice';
 
-const CampsitesList = ({ setCampsiteId }) => {
+const CampsitesList = () => {
     const campsites = selectAllCampsites();
-
     return (
         <Row className='ms-auto'>
             {campsites.map((campsite) => {
-                return(
-                    <Col
-                        md='5'
-                        className='m-4'
-                        key={campsite.id}
-                        onClick={() => setCampsiteId(campsite.id)}
-                    >
+                return (
+                    <Col md='5' className='m-4' key={campsite.id}>
                         <CampsiteCard campsite={campsite} />
                     </Col>
-                )
+                );
             })}
         </Row>
-    )
-}
+    );
+};
 
 export default CampsitesList;
