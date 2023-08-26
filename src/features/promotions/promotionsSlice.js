f7a9af009987e271e9ecfea92e5d631497447a20
an <anthedev@gmail.com>
2023-08-26 08:01:12 -0700

    corrects promotionsSlice.js

--- src/features/promotions/promotionsSlice.js
+++ src/features/promotions/promotionsSlice.js
@@ -13,5 +13,7 @@ const promotionsSlice = createSlice({
 export const promotionsReducer = promotionsSlice.reducer;
 
 export const selectFeaturedPromotion = (state) => {
-    return state.promotions.promotionsArray.find((promotion) => promotion.featured);
+    return state.promotions.promotionsArray.find(
+        (promotion) => promotion.featured
+    );
 };
