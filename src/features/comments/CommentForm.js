b388d4947d980a0a2bda7f054756d1578a750d8b
an <anthedev@gmail.com>
2023-08-26 07:25:16 -0700

    addComment

--- src/features/comments/CommentForm.js
+++ src/features/comments/CommentForm.js
@@ -9,18 +9,26 @@ import {
     FormGroup
 } from 'reactstrap';
 import { validateCommentForm } from '../../utils/validateCommentForm';
+import { useDispatch } from 'react-redux';
+import { addComment } from './commentsSlice';
 
 const CommentForm = ({ campsiteId }) => {
     const [modalOpen, setModalOpen] = useState(false);
+
+    const dispatch = useDispatch();
 
     const handleSubmit = (values) => {
         const comment = {
             campsiteId: parseInt(campsiteId),
             rating: values.rating,
             author: values.author,
-            text: values.commentText
+            text: values.commentText,
+            date: new Date(Date.now()).toISOString()
         };
         console.log('comment:', comment);
+
+        dispatch(addComment(comment));
+
         setModalOpen(false);
     };
 
