7ed3b2253ac80fe9382f20639e2d76a7ccdf48cc
an <anthedev@gmail.com>
2023-09-02 07:29:48 -0700

    async promotions

--- src/App.js
+++ src/App.js
@@ -11,17 +11,15 @@ import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import AboutPage from './pages/AboutPage';
 import { fetchCampsites } from './features/campsites/campsitesSlice';
 import { fetchPartners } from './features/partners/partnersSlice';
+import { fetchPromotions } from './features/promotions/promotionsSlice';
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchCampsites());
-  }, [dispatch]);
-
-  useEffect(() => {
-    dispatch(fetchCampsites());
     dispatch(fetchPartners());
+    dispatch(fetchPromotions());
   }, [dispatch]);
 
   return (
