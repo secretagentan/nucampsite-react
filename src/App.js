32cda10b2cffc8ad2d643c739275933a6bbd5ef3
an <anthedev@gmail.com>
2023-09-02 07:05:47 -0700

    async campsites

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
@@ -7,24 +9,32 @@ import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import AboutPage from './pages/AboutPage';
+import { fetchCampsites } from './features/campsites/campsitesSlice';
+
 
 function App() {
-    return (
-      <div className='App'>
-        <Header />
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='contact' element={<ContactPage />} />
-          <Route path='about' element={<AboutPage />} />
-          <Route path='directory' element={<CampsitesDirectoryPage />} />
-          <Route
-            path='directory/:campsiteId'
-            element={<CampsiteDetailPage />}
-          />
-        </Routes>
-        <Footer />
-      </div>
-    );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCampsites());
+  }, [dispatch]);
+
+  return (
+    <div className='App'>
+      <Header />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='about' element={<AboutPage />} />
+        <Route path='directory' element={<CampsitesDirectoryPage />} />
+        <Route
+          path='directory/:campsiteId'
+          element={<CampsiteDetailPage />}
+        />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
